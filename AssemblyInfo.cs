e9f4b5bc732ef48676161c2cb2e17c9b331ce52a
Redjive2 <60155382+Redjive2@users.noreply.github.com>
2025-01-30 23:23:15 -0600

    Add AssemblyInfo.cs
    I forgot this one

---
+++ AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using RumbleMod2;
+using MelonLoader;
+
+// ^C+^V from iLikeMyAccountName along with RumbleMod2.BuildInfo
+[assembly: AssemblyTitle(RumbleMod2.BuildInfo.Description)]
+[assembly: AssemblyDescription(RumbleMod2.BuildInfo.Description)]
+[assembly: AssemblyCompany(RumbleMod2.BuildInfo.Company)]
+[assembly: AssemblyProduct(RumbleMod2.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + RumbleMod2.BuildInfo.Author)]
+[assembly: AssemblyTrademark(RumbleMod2.BuildInfo.Company)]
+[assembly: AssemblyVersion(RumbleMod2.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(RumbleMod2.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(NoMute), RumbleMod2.BuildInfo.Name, RumbleMod2.BuildInfo.Version, RumbleMod2.BuildInfo.Author, RumbleMod2.BuildInfo.DownloadLink)]
+
+[assembly: MelonGame("Buckethead Entertainment", "RUMBLE")]
