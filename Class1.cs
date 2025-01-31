051aa8ff6ff7bc4675d2b86ade46500ce35962ac
Redjive2 <60155382+Redjive2@users.noreply.github.com>
2025-01-30 23:22:42 -0600

    Initial
    No idea if it works lol

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using MelonLoader;
+using RumbleModdingAPI;
+
+namespace RumbleMod2
+{
+    public static class BuildInfo
+    {
+        public const string Name = "NoMute"; // Name of the Mod.  (MUST BE SET)
+        public const string Description = "no more muting people on accident"; // Description for the Mod.  (Set as null if none)
+        public const string Author = "rdm8417"; // Author of the Mod.  (MUST BE SET)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class NoMute : MelonMod
+    {
+        private bool loaded;
+        
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (sceneName == "Gym" && !loaded)
+            {
+                loaded = true;
+                
+                MelonLogger.Msg("NoMute initialized");
+            
+                Calls.Managers.GetInputManager().mutePlayerActionName = "whatever";
+            
+                MelonLogger.Msg("NoMute active");
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+﻿# NoMute
+Version 1.0.0
+<br>
+By rdm8417
+
+Disables the mute button
+<br>
+Truly, a great leap forward for rumblers everywhere
